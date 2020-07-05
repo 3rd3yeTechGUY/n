100162a92e0c064d020c8fdb0a3555f5f93cd01e
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-07-05 22:42:50 +0530

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -14,6 +14,7 @@ echo -e $'\e[1;33m\e[0m\e[1;91m ██████  █████   █�
 echo -e $'\e[1;33m\e[0m\e[1;91m ██   ██ ██      ██   ██ \e[0m''\e[1;33m\e[0m\e[1;92m    ██         ██    ██ \e[0m'   '\e[1;33m\e[0m\e[1;93m \e[0m'
 echo -e $'\e[1;33m\e[0m\e[1;91m ██   ██ ███████ ██████ \e[0m''\e[1;33m\e[0m\e[1;92m     ███████    ██    ███████ \e[0m''\e[1;33m\e[0m\e[1;93m v1.0 \e[0m'
 echo ""
-echo -e $'\e[1m\e[32m        ▂▃▄▅▆▇▓▒░Coded By \e[31mTECH COCHI \e[1m\e[32m░▒▓▇▆▅▄▃▂'
+echo ""
+echo -e $'\e[1m\e[96m        ▂▃▄▅▆▇▓▒░"\e[38;5;82mTECH \e[38;5;198mCOCHI"\e[96m░▒▓▇▆▅▄▃▂"
 }
 banner
