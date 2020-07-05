90b192ce5a007758d1b5334d805f0bfb98f52d9e
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-07-05 21:52:55 +0530

    Create log.sh

---
+++ log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+echo ""
+echo""
+banner() {
+echo -e $'\e[1;33m\e[0m\e[1;91m ██████  ███████ ██████ \e[0m''\e[1;33m\e[0m\e[1;92m     ███████ ██    ██ ███████ \e[0m''\e[1;33m\e[0m\e[1;93 \e[0m'
+echo -e $'\e[1;33m\e[0m\e[1;91m ██   ██ ██      ██   ██ \e[0m''\e[1;33m\e[0m\e[1;92m    ██       ██  ██  ██ \e[0m'   '\e[1;33m\e[0m\e[1;93m Φ TECH COCHI \e[0m'
+echo -e $'\e[1;33m\e[0m\e[1;91m ██████  █████   ██   ██ \e[0m''\e[1;33m\e[0m\e[1;92m    █████     ████   █████ \e[0m''\e[1;33m\e[0m\e[1;93m \e[0m'
+echo -e $'\e[1;33m\e[0m\e[1;91m ██   ██ ██      ██   ██ \e[0m''\e[1;33m\e[0m\e[1;92m    ██         ██    ██ \e[0m'   '\e[1;33m\e[0m\e[1;93m \e[0m'
+echo -e $'\e[1;33m\e[0m\e[1;91m ██   ██ ███████ ██████ \e[0m''\e[1;33m\e[0m\e[1;92m     ███████    ██    ███████ \e[0m''\e[1;33m\e[0m\e[1;93m v1.0 \e[0m'
+echo ""
+echo -e $'\e[1m\e[32m        ▂▃▄▅▆▇▓▒░Coded By \e[31mTECH COCHI \e[1m\e[32m░▒▓▇▆▅▄▃▂'
+}
+banner
